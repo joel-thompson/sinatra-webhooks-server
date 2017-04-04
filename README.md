9e76e92e048914f3f7b445a6a259e5a5d05bcadf
Joel Thompson <joel.thompson@intercom.io>
2017-04-04 11:59:19 -0700

    updated to work with signed webhooks

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # sinatra-webhooks-server
 Simple sinatra script from testing webhooks and such
+
+Used with ngrok to test signed and unsigned webhooks.
+
+run this:
+
+```
+ngrok http 4567
+```
+
+followed by this:
+
+```
+ruby webhooks_server.rb
+```
--- webhooks_server.rb
+++ webhooks_server.rb
@@ -5,14 +5,47 @@ require 'json'
 #It automatically binds to 127.0.0.0 
 # set :bind, '0.0.0.0'
 
-payload = ""
+global_payload = ""
 
+# receives unsigned webhooks and populates the unsigned_payload_body global variable
 post '/webhook' do
   push = JSON.parse(request.body.read)
   puts "Webhook JSON Data: #{push.inspect}"
-  payload = push.inspect
+  unsigned_payload_body = push.inspect
+  global_payload = unsigned_payload_body
 end
 
+# simply prints out the global_payload
 get '/show_webhook' do
-	payload
+	global_payload
+end
+
+
+# expects a signed webhook, returns 500 if the signatures don't match
+post '/webhook_signed' do
+	request.body.rewind
+ 	signed_payload_body = request.body.read
+ 	verify_signature(signed_payload_body)
+ 	push = JSON.parse(signed_payload_body)
+ 	puts "Webhook JSON Data: #{push.inspect}"
+ 	global_payload = signed_payload_body
+end
+
+# needs commenting =)
+def verify_signature(payload_body)
+  secret = "poo"
+  expected = request.env['HTTP_X_HUB_SIGNATURE']
+  if expected.nil? || expected.empty? then
+    puts "Not signed. Not calculating"
+  else
+    signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), secret, payload_body)
+    puts "Expected  : #{expected}"
+    puts "Calculated: #{signature}"
+    if Rack::Utils.secure_compare(signature, expected) then
+      puts "   Match"
+    else
+      puts "   MISMATCH!!!!!!!"
+      return halt 500, "Signatures didn't match!"
+    end
+  end
 end
